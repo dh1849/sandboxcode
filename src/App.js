92d9714c5924c2be9978aeb0a3c90d37ecd8b527
dh1849 <dh1849@yahoo.com>
2021-03-11 22:40:46 -0500

    figured out exec() returned object array indices

--- src/App.js
+++ src/App.js
@@ -4,23 +4,47 @@ import "./styles.css";
 export default function App() {
   const url = "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
 
-  const parsedURL = /^(\w+):\/\/([^\/]+)\/(.*)$/.exec(url);
+  const parsedURL = /^(\w+)\:\/\/([^\/]+)\/(.*)$/.exec(url);
+
+  // Note: Returned object of exec() contains an array of elements =
+  // # of parenthesized substrings +1 = number of ()+1
+  // escape special characters: \:, \/, \/,\/
+
+  // index[0]=full string of characters matched="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+  // index[1]=(\w+)="https", 1 or more word character
+  // index[2]=([^\/]+)= "developer.mozilla.org" start with 1 or more /
+  // index[3]=(.*)$ = "en-US/docs/Web/JavaScript" ending with 0 or more .
+
+  // returned parsedURL: ["https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  // "https", "developer.mozilla.org", "en-US/docs/Web/JavaScript"]
+
+  // const parsedURL = /[+-]?(\d+(\.\d+)?|\.\d+)([eE][+-]?\d+)?/.exec(url);
+  // const url = "-1000.3454";
+  // const parsedURL = /[+-]?(\d+)(\.\d+)/g.exec(url);
+  // the object returned two element:
+  // index[0]=-1000.3454  full string of matched characters
+  // index[1]=1000.3454 = (\d+(\.\d+)?|\.\d+)
+  // index[1] only include strings inside() without "-" sign
+
   if (!parsedURL) {
     return false;
   }
-  // syntax /pattern/modifiers; eg: const patt = /w3schools/i
-  // i: match case-insensitive; g: global match (match all not stopping after first match)
-  // m: multiple matching; [abc] any character between brackets;
-  // [^abc] any character NOT between brackets; [0-9] any digit between brackets;
-  // [^0-9] any none-digit; (x|y) any alternatives specified
-  // . a single character except newline or line terminator; \w a word character;
-  // \W (capital): a non-word character; \d a gigit; \D a non-digit;
+
+  // Metacharacter: ^ starting and $ ending position; () block of string inside() can be recalled
+  // + 1 or more; * 0 or more preceding element; ? 0 or 1 preceding element (eg: colou?r matches both color and colour)
+  // {n} match exactly n times; {min,}, {,max}, {min,max} match at least min times but NOT more than max times
+  // syntax /pattern/modifiers; eg: const patt = /w3schools/i; i: match case-insensitive;
+  //  g: global match (match all not stopping after first match); m: multiple matching;
+  // [^abc] any single character NOT between brackets; [abc] any single character between brackets;
+  //  [0-9] any digit between brackets; [^0-9] any none-digit; (x|y) any alternatives specified
+  // . match any single character except newline or line terminator; \w a word [A-Za-z0-9_] or alphanumeric character;
+  // \W (capital): a non-word character; \d single gigit; \D a non-digit;
   // \s a whitespace character; \S a non-whitespace character; \b match at beginning/end of a word;
   // beginning like: \bHI and end like: HI\b; \B match in-between beginning/end of a word;
   // \0 a null character; \n newline; \f a form feed character(FF page-break ASCII control character of 12);
   // \r return character; \t tab character; \v vertical tab(VT for vertical line movement);
   // \xxx octal number xxx; \xdd hexadecimal number dd; \udddd unicode character by hexadecimal dddd;
-
+  // n is a digit from 1 to 9;
   // n+ string with at least 1 n; n* string with 0 or more n; n? 0 or 1 n;
   // n{X} string with a sequence of X of n's; n{X,Y} string with a sequence of X to Y of n's;
   // n{X,} string with a sequence of at least X n's; n$ string with n at the end of string;
@@ -30,10 +54,12 @@ export default function App() {
   // toString() return the string value; test() return true or false for matching;
   // exec() return only the first match
 
-  // ["https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-  //    "https", "developer.mozilla.org", "en-US/docs/Web/JavaScript"]
+  // ignore: index[0]=["https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  // index[1]=protocol="https", index[2]=fullhost="developer.mozilla.org",
+  // index[3]=fullpath="en-US/docs/Web/JavaScript"]
 
   const [, protocol, fullhost, fullpath] = parsedURL;
+
   return (
     <div>
       <h1>{parsedURL}</h1>
